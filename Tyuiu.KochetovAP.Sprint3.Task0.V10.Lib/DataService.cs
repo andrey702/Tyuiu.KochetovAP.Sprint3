363b056f0743f07b28ba13542df28a9f50074ef1
andreykochetov <oliverr223344@gmail.com>
2025-10-12 21:50:20 +0600

    Add project files.

---
+++ Tyuiu.KochetovAP.Sprint3.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KochetovAP.Sprint3.Task0.V10.Lib
+{
+    public class DataService : ISprint3Task0V10
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+
+            double p = 1;
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                p *= Math.Pow(300.0 / (i + value), i);
+            }
+
+            return Math.Round(p, 3);
+
+
+        }
+    }
+}
---
+++ Tyuiu.KochetovAP.Sprint3.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KochetovAP.Sprint3.Task0.V10.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint3.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double result = ds.GetMultiplySeries(5, 1, 5);
+
+            double expected = 1.0;
+            for (int i = 1; i <= 5; i++)
+            {
+                expected *= Math.Pow(300.0 / (i + 5), i);
+            }
+            expected = Math.Round(expected, 3);
+
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}

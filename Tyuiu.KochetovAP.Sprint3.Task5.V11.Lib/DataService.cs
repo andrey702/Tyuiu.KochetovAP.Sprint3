088dab08a4d7131255a59d9ecc5c927249e4f3f5
andreykochetov <oliverr223344@gmail.com>
2025-10-13 22:07:20 +0600

    Task5

---
+++ Tyuiu.KochetovAP.Sprint3.Task5.V11.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KochetovAP.Sprint3.Task5.V11.Lib
+{
+    public class DataService : ISprint3Task5V11
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double y = 0;
+
+            
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    
+                    y += Math.Sin(k);
+                }
+            }
+
+            
+            y += x / 2.0;
+
+            
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.KochetovAP.Sprint3.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KochetovAP.Sprint3.Task5.V11.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint3.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService ds = new DataService();
+
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+
+            int x = 5;
+            int startValue1 = 1;
+            int stopValue1 = 3;
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            double result = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+
+            double wait = 6.734; 
+            Assert.AreEqual(wait, result);
+        }
+    }
+    
+}
